d9cf52f91541cea1bf7e6561d410ec8731a1859c
prashaant nisal 5050 <pnisal5050@gmail.com>
2025-06-11 01:26:04 +0530

    app.rb modify

--- server/app.rb
+++ server/app.rb
@@ -3,22 +3,32 @@ require 'sinatra'
 require 'json'
 require 'sinatra/cross_origin'
 require_relative './add'
-require 'byebug'
-set :public_folder, File.expand_path('../../client/build', __FILE__)
 
-configure do
-  enable :cross_origin
-end
+# === Static File Setup ===
+set :public_folder, File.expand_path('../client/build', __dir__)
+set :static, true
+enable :cross_origin
 
+# === Serve React App ===
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
+# Handle all other client-side routes (for SPA routing)
+get '/*' do
+  if File.exist?(File.join(settings.public_folder, request.path_info))
+    send_file File.join(settings.public_folder, request.path_info)
+  else
+    send_file File.join(settings.public_folder, 'index.html')
+  end
+end
+
+# === API Routes ===
 before do
   content_type :json
 end
 
-# Allow preflight requests (important for POST requests from browsers)
+# Allow preflight requests for CORS
 options '*' do
   response.headers['Access-Control-Allow-Origin'] = '*'
   response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
@@ -26,8 +36,10 @@ options '*' do
   200
 end
 
+# POST /add
 post '/add' do
   headers 'Access-Control-Allow-Origin' => '*'
+
   begin
     input = JSON.parse(request.body.read)['input']
     result = StringCalculator.new(input).add
