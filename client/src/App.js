460dd55e9d8c4c6411cfa987870263342e509076
prashaant nisal 5050 <pnisal5050@gmail.com>
2025-06-11 12:31:12 +0530

    issue fix

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,13 @@ function App() {
 
   const handleCalculate = async () => {
     try {
-      const response = await fetch("http://localhost:4567/add", {
+      const API_URL =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:4567/add"
+        : "/add";
+
+      
+      const response = await fetch("API_URL", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ input })
